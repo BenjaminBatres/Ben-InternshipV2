22075871cbd7de6f52ae789121aac38fd979653a
Ben <benbatres1212@gmail.com>
2025-06-07 08:52:35 -0700

    added settings page

---
+++ src/app/settings/page.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='wrapper'>
+      <div className="container">
+        <div className="row">
+          <div className="section__title page__title">Settings</div>
+          <div className="setting__content">
+            <div className="settings__sub--title">
+              Your Subscription plan
+            </div>
+            <div className="settings__text">premium</div>
+          </div>
+          <div className="setting__content">
+            <div className="settings__sub--title">
+              Email
+            </div>
+            <div className="settings__text">hanna@gmail.com</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
